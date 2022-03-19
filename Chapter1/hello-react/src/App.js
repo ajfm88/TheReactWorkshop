240cfd43eb9ff1b970bfd6faaf8ccfc6bfb11735
ajfm88 <ajfoucaultmo@mail.usf.edu>
2022-03-18 19:29:32 -0700

    Exercise 1.02: Creating Our First React Component

--- Chapter1/hello-react/src/App.js
+++ Chapter1/hello-react/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 
-const Greeting = props => <p>Hello {props.name}!</p>
-
 const App = props => (
   <div>
     <h1>My App</h1>
     <br />
-    <Greeting name="Hideo Kojima" />
+    Hello React!
   </div>
-)
-
+);
 export default App;
