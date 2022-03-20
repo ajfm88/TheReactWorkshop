a574a6c3cbda6e3f5d485d87d8806517c9baf7e9
ajfm88 <ajfoucaultmo@mail.usf.edu>
2022-03-19 22:20:58 -0700

    Exercise 1.03: Implementing a Click Counter in React Using Classes

--- Chapter1/hello-react/src/App.js
+++ Chapter1/hello-react/src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
-
-const App = props => (
-  <div>
-    <h1>My App</h1>
-    <br />
-    Hello React!
-  </div>
-);
+import React, { Component } from 'react';
+ 
+const Greeting = props => <p>Hello {props.name}!</p>;
+ 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.title = 'React App';
+    this.state = { clickCounter: 0 };
+  }
+  renderClickCount() {
+    return <p>I've been clicked {this.state.clickCounter} times!</p>;
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.title}</h1>
+        <br />
+        <Greeting name="User" />
+        <br />
+        {this.renderClickCount()}
+      </div>
+    );
+  }
+}
+ 
 export default App;
