7b0af2f1cde604a216470475b500eb06460105f3
ajfm88 <ajfoucaultmo@mail.usf.edu>
2022-03-21 16:01:23 -0700

    Exercise 2.04: Using Alternative Class Declarations to Avoid Binds

--- Chapter2/sign-up-form/src/App.js
+++ Chapter2/sign-up-form/src/App.js
@@ -1,38 +1,36 @@
-import React, {Component} from 'react';
-
+import React, { Component } from "react";
 class App extends Component {
   state = {
-    username: '',
-    password: '',
-    passwordConfirmation: '',
-    email: '',
-    errors: []
+    username: "",
+    password: "",
+    passwordConfirmation: "",
+    email: "",
+    errors: [],
   };
-  validateUsernameOnBlur = this.validateUsernameOnBlur.bind(this);
-  
+  validateUsernameOnBlur = (event) => {
+    console.log("I should validate whatever is in ", event.target.value);
+    this.setState();
+  };
   displayForm() {
     return (
       <div>
-        Username: <input type="text" onBlur={this.validateUsernameOnBlur} /><br />
-        Password: <input type="text" /><br />
-        Password Confirmation: <input type="text" /><br />
-        Email: <input type="text" /><br />
+        Username: <input type="text" onBlur={this.validateUsernameOnBlur} />
+        <br />
+        Password: <input type="text" />
+        <br />
+        Password Confirmation: <input type="text" />
+        <br />
+        Email: <input type="text" />
+        <br />
         <br />
         <button onClick={this.submitForm}>Submit</button>
       </div>
     );
   }
-
-  validateUsernameOnBlur(event) {
-    console.log("I should validate whatever is in ", event.target.value);
-    this.setState();
-  }
-
   submitForm(event) {
     console.log("Submitting the form now...");
     console.log(event);
   }
-
   render() {
     return (
       <div className="App">
@@ -40,9 +38,7 @@ class App extends Component {
         <hr />
         {this.displayForm()}
       </div>
-    )
+    );
   }
-
 }
-
 export default App;
