f994c8674d8dec81999422dab1e0c6d62150862a
ajfm88 <ajfoucaultmo@mail.usf.edu>
2022-04-04 20:39:07 -0700

    Exercise 7.01: Sending and Receiving Objects as Props from the Parent

--- Chapter7/animal-endangered/src/App.js
+++ Chapter7/animal-endangered/src/App.js
@@ -1,34 +1,43 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
   render() {
-    return <Animal name="Tiger" number={3890} endangered={true} />;
+    const details = {
+      name: 'Tiger',
+      number: 3890,
+      endangered: true
+    };
+    return <Animal details={details} />;
+    // return <Animal {...details} />;
   }
 }
 
 // Class-based component
-// class Animal extends Component {
-//   render({ name, number, endangered } = this.props) {
-//     return  (
-//       <div>
+export class Animal extends Component {
+  render() {
+    const { name, number, endangered } = this.props.details;
+    // const { name, number, endangered } = props;
+
+    return (
+      <div>
+        <p>Animal: {name}</p>
+        <p>Number: {number}</p>
+        <p>Endangered: {endangered ? 'Yes' : 'No'}</p>
+      </div>
+    );
+  }
+}
+
+// Functional component
+// const Animal = props => {
+//   const { name, number, endangered } = props.details;
+//   return (
+//     <div>
 //       <p>Animal: {name}</p>
 //       <p>Number: {number}</p>
 //       <p>Endangered: {endangered ? 'Yes' : 'No'}</p>
-//       </div>
-//     );
-//   };
+//     </div>
+//   );
 // };
-
-// // Functional component
-const Animal = ({ name, number, endangered }) => {
-  return (
-    <div>
-      <p>Animal: {name}</p>
-      <p>Number: {number}</p>
-      <p>Endangered: {endangered ? 'Yes' : 'No'}</p>
-    </div>
-  );
-};
 
 export default App;
