a1404c63c6465996ff7b30ec7c35ad1c67066562
ajfm88 <ajfoucaultmo@mail.usf.edu>
2022-04-06 15:47:37 -0700

    Exercise 7.02: Sending Child Elements Using the children Prop

--- Chapter7/animal-endangered/src/App.js
+++ Chapter7/animal-endangered/src/App.js
@@ -2,42 +2,25 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    const details = {
-      name: 'Tiger',
-      number: 3890,
-      endangered: true
-    };
-    return <Animal details={details} />;
-    // return <Animal {...details} />;
-  }
-}
-
-// Class-based component
-export class Animal extends Component {
-  render() {
-    const { name, number, endangered } = this.props.details;
-    // const { name, number, endangered } = props;
-
     return (
-      <div>
-        <p>Animal: {name}</p>
-        <p>Number: {number}</p>
-        <p>Endangered: {endangered ? 'Yes' : 'No'}</p>
-      </div>
+      <Animal name='Tiger' number={3890} endangered={true}>
+        <h1>Endangered Animals</h1>
+      </Animal>
     );
   }
 }
 
-// Functional component
-// const Animal = props => {
-//   const { name, number, endangered } = props.details;
-//   return (
-//     <div>
-//       <p>Animal: {name}</p>
-//       <p>Number: {number}</p>
-//       <p>Endangered: {endangered ? 'Yes' : 'No'}</p>
-//     </div>
-//   );
-// };
+const Animal = props => {
+  const { name, number, endangered, children } = props;
+
+  return (
+    <div>
+      {children}
+      <p>Animal: {name}</p>
+      <p>Number: {number}</p>
+      <p>Endangered: {endangered ? 'Yes' : 'No'}</p>
+    </div>
+  );
+};
 
 export default App;
